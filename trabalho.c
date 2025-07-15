bcb93e67a04aecfb8c22bb3102621f6827b2be08
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2025-07-14 22:04:00 -0300

    feat: colocando estrutura para chamar as funções

--- trabalho.c
+++ trabalho.c
@@ -4,7 +4,7 @@
 int main(){
     int N, M, H, T, I, C, P, Q;
     char op;
-    int des;
+    int X, D;
     scanf("%d %d %d %d %d %d %d", &N, &M, &H, &T, &I, &C, &P);
     int o[P], u[M], v[M], w[M];
     for(int i = 0; i < P; i++){
@@ -22,6 +22,29 @@ int main(){
         for(int n = 0; n < k[m]; n++){
             scanf("%d", &d[m][n]);
     }
-    scanf("%c %d", &op, &des);
+    scanf("%c", &op);
+    if(op == 'm')
+    {
+        scanf("%d", &X);
+        // Função de mover para X
+    }
+    else if (op == 'a')
+    {
+        // Função de abastecer
+    }
+    else if(op == 'p')
+    {
+        scanf("%d", &D);
+        // Função pegar pedido D
+    }
+    else if (op == 'o')
+    {
+        scanf("%d", &D);
+        // Função entregar pedido no local D
+    }
+    else if(op == 'x')
+    {
+        // Função de encerrar a entrega
+    }
     return 0;
 }
