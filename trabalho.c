1624b43494c52ed579a03f297621ff9a486dcdf7
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2025-07-23 17:00:56 -0300

    fudeu

--- trabalho.c
+++ trabalho.c
@@ -3,221 +3,275 @@
 #include <limits.h>
 #include <stdbool.h>
 
+#define MAXN 1000
+
 typedef struct node *link;
-struct node{int w, custo; link next;};
+struct node { int w, custo; link next; };
 
 typedef struct Graph *Graph;
 struct Graph {
     int v, e;
     link *adj;
+    bool *temPosto;
+    bool *temRestaurante;
 };
 
-typedef struct{
+typedef struct {
     int local, numPedidos, *destinoPedido;
-}Restaurante;
+} Restaurante;
 
 typedef struct {
     int v, w, custo;
 } Edge;
 
 Edge EDGE(int v, int w, int custo) {
-    Edge e;
-    e.v = v;
-    e.w = w;
-    e.custo = custo;
+    Edge e = {v, w, custo};
     return e;
 }
 
-link New(int w, int custo, link next){
+link New(int w, int custo, link next) {
     link x = malloc(sizeof(*x));
-    if(x==NULL) return NULL;
-    x->w = w; 
-    x->custo = custo;
-    x->next = next;
+    if (!x) return NULL;
+    x->w = w; x->custo = custo; x->next = next;
     return x;
 }
 
-Graph GRAPHInit(int v){
+Graph GRAPHInit(int v) {
     Graph g = malloc(sizeof(*g));
-    g->v = v;
-    g->e=0;
-    g->adj = malloc(v*sizeof(link));
-    for(int i=0; i < v; i++){
-        g->adj[i]=NULL;
-    }
+    g->v = v; g->e = 0;
+    g->adj = calloc(v, sizeof(link));
+    g->temPosto = calloc(v, sizeof(bool));
+    g->temRestaurante = calloc(v, sizeof(bool));
     return g;
 }
 
-void GRAPHInsert(Graph g, Edge e){
-    int v=e.v, w=e.w, custo=e.custo;
-    g->adj[v] = New(w, custo, g->adj[v]);
+void GRAPHInsert(Graph g, Edge e) {
+    g->adj[e.v] = New(e.w, e.custo, g->adj[e.v]);
+    g->adj[e.w] = New(e.v, e.custo, g->adj[e.w]);
     g->e++;
 }
 
-void Dijkstra(Graph G, int s, int *pa, int *distD){
-    bool mature[1000];
-    for(int v = 0; v < G->v; v++) {
-        pa[v] = -1;
-        mature[v] = false;
-        distD[v] = INT_MAX;
+void Dijkstra(Graph G, int s, int *pa, int *dist) {
+    bool vis[MAXN] = {0};
+    for (int i = 0; i < G->v; i++) {
+        dist[i] = INT_MAX;
+        pa[i] = -1;
     }
-    distD[s] = 0;
-    while(true){
-        int min = INT_MAX;
-        int y = -1;
-        for(int z = 0; z < G->v; z++){
-            if(!mature[z] && distD[z] < min){
-                min = distD[z];
-                y = z;
+    dist[s] = 0;
+
+    while (1) {
+        int u = -1;
+        for (int i = 0; i < G->v; i++) {
+            if (!vis[i] && (u == -1 || dist[i] < dist[u]))
+                u = i;
+        }
+        if (u == -1 || dist[u] == INT_MAX) break;
+        vis[u] = true;
+        for (link p = G->adj[u]; p; p = p->next) {
+            if (dist[u] + p->custo < dist[p->w]) {
+                dist[p->w] = dist[u] + p->custo;
+                pa[p->w] = u;
             }
         }
-        if(y == -1)
-            break;
-        for(link a = G->adj[y]; a != NULL; a = a->next){
-            int w = a->w;
-            int custo = a->custo;
-            if(!mature[w] && distD[y] + custo < distD[w]){
-                distD[w] = distD[y] + custo;
-                pa[w] = y;
-            }
-        }
-        mature[y] = true;
     }
 }
 
-int moveLocal(int localMove, int N, Graph G, int localDestinoMove, int combustivel,int *distM){
-    int pa[N];
-    int distMInterno[N];
-    Dijkstra(G, localMove, pa, distMInterno);
-    if(distMInterno[localDestinoMove] <= combustivel){
-        *distM = distMInterno[localDestinoMove];
-        return 1;
-    }
-    return 0;
-}
-
-int abastecer(int localPostos, int *postosAbastecimento, int *combustivelAtual, int combustivelMaximo){
-    if(postosAbastecimento[localPostos] == 1){
-        *combustivelAtual = combustivelMaximo;
-        return *combustivelAtual;
-    }
-    return 0;
-}
-
-int coletaPedido(int pedido){
-    return pedido;
-}
-
-int entregaPedido(int pedido){
-    return pedido;
-}
-
-int *postos;
+int N, M, H, T, I, C, P, Q;
 Restaurante *restaurantes;
-int capacidadeCombustivel, combustivel, capacidadeMochila;
-link ultimoLocal;
+Graph G;
 
-int main(){
-    int N, M, H, T, I, C, P, Q;
-    char op;
-    int X, D;
-    Graph G;
+int mochila[MAXN], mochilaAtual = 0;
+int posicaoAtual, combustivelAtual;
 
-    scanf("%d %d %d %d %d %d %d", &N, &M, &H, &T, &I, &C, &P);
-    capacidadeCombustivel = T;
-    combustivel = I;
-    capacidadeMochila = C;
+void emitir(const char *fmt, int x) {
+    if (x >= 0) printf(fmt, x + 1);
+    else printf("%s", fmt);
+    printf("\n");
+    fflush(stdout);
+}
 
+int ler() {
+    int r;
+    if (scanf("%d", &r) != 1) exit(1);
+    return r;
+}
+
+bool moverPara(int destino, int *pai, int *dist) {
+    if (destino < 0 || destino >= N) return false;
+
+    if (dist[destino] > combustivelAtual) {
+        int melhorPosto = -1, menorCusto = INT_MAX;
+        for (int i = 0; i < N; i++) {
+            if (!G->temPosto[i]) continue;
+            if (dist[i] <= combustivelAtual && dist[i] < menorCusto) {
+                melhorPosto = i;
+                menorCusto = dist[i];
+            }
+        }
+        if (melhorPosto != -1) {
+            moverPara(melhorPosto, pai, dist);
+            emitir("a", -1);
+            combustivelAtual = ler();
+            return moverPara(destino, pai, dist);
+        }
+        return false;
+    }
+
+    if (pai[destino] != -1) moverPara(pai[destino], pai, dist);
+    emitir("m %d", destino);
+    int r = ler();
+    if (r == 1) {
+        for (link a = G->adj[posicaoAtual]; a; a = a->next)
+            if (a->w == destino) combustivelAtual -= a->custo;
+        posicaoAtual = destino;
+
+        if (G->temPosto[posicaoAtual]) {
+            emitir("a", -1);
+            combustivelAtual = ler();
+        }
+
+        return true;
+    }
+    return false;
+}
+
+bool pegarPedido(int destino) {
+    if (destino < 0 || destino >= N) return false;
+    emitir("p %d", destino);
+    int r = ler();
+    if (r == 1) mochila[mochilaAtual++] = destino;
+    return r == 1;
+}
+
+bool entregarPedido(int destino) {
+    if (destino < 0 || destino >= N) return false;
+    emitir("e %d", destino);
+    int r = ler();
+    if (r == 1) {
+        for (int i = 0; i < mochilaAtual; i++) {
+            if (mochila[i] == destino) {
+                for (int j = i; j < mochilaAtual - 1; j++)
+                    mochila[j] = mochila[j + 1];
+                mochilaAtual--;
+                break;
+            }
+        }
+    }
+    return r == 1;
+}
+
+int encontrarEntregaMaisProxima(int *pai, int *dist) {
+    int distLocal[MAXN], paiLocal[MAXN];
+    Dijkstra(G, posicaoAtual, paiLocal, distLocal);
+    int melhor = -1, menorDist = INT_MAX;
+    for (int i = 0; i < mochilaAtual; i++) {
+        int destino = mochila[i];
+        if (destino >= 0 && destino < N && distLocal[destino] < menorDist) {
+            menorDist = distLocal[destino];
+            melhor = destino;
+        }
+    }
+    if (melhor != -1) {
+        for (int i = 0; i < N; i++) {
+            pai[i] = paiLocal[i];
+            dist[i] = distLocal[i];
+        }
+    }
+    return melhor;
+}
+
+void encerrar() {
+    emitir("x", -1);
+    ler();
+}
+
+int main() {
+    if (scanf("%d %d %d %d %d %d %d", &N, &M, &H, &T, &I, &C, &P) != 7) exit(1);
     G = GRAPHInit(N);
-    if(!G){
-        return -1;
+    for (int i = 0; i < P; i++) {
+        int p; if (scanf("%d", &p) != 1) exit(1);
+        G->temPosto[p - 1] = true;
     }
-
-    postos = malloc(P * sizeof(int));
-    if(!postos){
-        return -1;
+    for (int i = 0; i < M; i++) {
+        int u, v, w;
+        if (scanf("%d %d %d", &u, &v, &w) != 3) exit(1);
+        GRAPHInsert(G, EDGE(u - 1, v - 1, w));
     }
-    for(int i = 0; i < P; i++){
-        postos[i] = 0;
-    }
-
-    int posto = -1;
-    for(int i = 0; i < P; i++){
-        scanf("%d", &posto);
-        postos[posto] = 1;
-    }
-
-    int u, v, w;
-    for(int j = 0; j < M; j++){
-        scanf("%d %d %d", &u, &v, &w);
-        GRAPHInsert(G, EDGE(u, v, w));
-    }
-
-    scanf("%d", &Q);
+    if (scanf("%d", &Q) != 1) exit(1);
     restaurantes = malloc(Q * sizeof(Restaurante));
-    if(!restaurantes){
-        return -1;
+    if (!restaurantes) exit(1);
+    for (int i = 0; i < Q; i++) {
+        int r, k;
+        if (scanf("%d %d", &r, &k) != 2) exit(1);
+        G->temRestaurante[r - 1] = true;
+        restaurantes[i].local = r - 1;
+        restaurantes[i].numPedidos = k;
+        restaurantes[i].destinoPedido = malloc(k * sizeof(int));
+        if (!restaurantes[i].destinoPedido) exit(1);
+        for (int j = 0; j < k; j++) {
+            if (scanf("%d", &restaurantes[i].destinoPedido[j]) != 1) exit(1);
+            restaurantes[i].destinoPedido[j]--;
+        }
     }
+    posicaoAtual = H - 1;
+    combustivelAtual = I;
 
-    int r, k;
-    for(int m = 0; m < Q; m++){
-        scanf("%d %d", &r, &k);
-        restaurantes[m].local = r;
-        restaurantes[m].numPedidos = k;
-        restaurantes[m].destinoPedido = malloc(k*sizeof(int));
-        for(int n = 0; n < k; n++)
-            scanf("%d", &restaurantes[m].destinoPedido[n]);
-    }
+    while (1) {
+        int pai[MAXN], dist[MAXN];
+        Dijkstra(G, posicaoAtual, pai, dist);
 
-    int posicaoAtual = H, combustivelAtual = I;
-    while(1){
-        scanf(" %c", &op);
-        int mov, abs, res;
-        switch (op)
-        {
-            case 'm':
-                scanf("%d", &X);
-                int distMov;
-                mov = moveLocal(posicaoAtual, N, G, X, combustivelAtual, &distMov);
-                printf("%d\n", mov);
-                if(mov != 0){
-                    combustivelAtual -= distMov;
-                    posicaoAtual = X;
+        int idx = -1, bestScore = INT_MAX;
+        for (int i = 0; i < Q; i++) {
+            if (restaurantes[i].numPedidos == 0) continue;
+            int rloc = restaurantes[i].local;
+            int score = dist[rloc];
+            int pdist[MAXN], ppai[MAXN];
+            Dijkstra(G, rloc, ppai, pdist);
+            for (int j = 0; j < restaurantes[i].numPedidos; j++) {
+                int d = restaurantes[i].destinoPedido[j];
+                if (d >= 0 && d < N && pdist[d] < INT_MAX) score += pdist[d];
+            }
+            if (score < bestScore) {
+                bestScore = score;
+                idx = i;
+            }
+        }
+
+        if (idx == -1) break;
+        int rlocal = restaurantes[idx].local;
+        moverPara(rlocal, pai, dist);
+
+        while (restaurantes[idx].numPedidos > 0 && mochilaAtual < C) {
+            int melhorD = -1, melhorDist = INT_MAX;
+            int distTemp[MAXN], paiTemp[MAXN];
+            Dijkstra(G, rlocal, paiTemp, distTemp);
+            for (int i = 0; i < restaurantes[idx].numPedidos; i++) {
+                int d = restaurantes[idx].destinoPedido[i];
+                if (d >= 0 && d < N && distTemp[d] < melhorDist) {
+                    melhorDist = distTemp[d];
+                    melhorD = i;
                 }
-                break;
-            case 'a':
-                abs = abastecer(posicaoAtual, postos, &combustivelAtual, T);
-                printf("%d\n", abs);
-                break;
-            case 'p':
-                scanf("%d", &D);
-                res = coletaPedido(D);
-                printf("%d", res);
-                break;
-            case 'o':
-                scanf("%d", &D);
-                res = entregaPedido(D);
-                printf("%d", res);
-                break;
-            case 'x':
-                printf("1\n");
-                return 0;
+            }
+            if (melhorD == -1) break;
+            int destino = restaurantes[idx].destinoPedido[melhorD];
+            if (!pegarPedido(destino)) break;
+            for (int j = melhorD + 1; j < restaurantes[idx].numPedidos; j++)
+                restaurantes[idx].destinoPedido[j - 1] = restaurantes[idx].destinoPedido[j];
+            restaurantes[idx].numPedidos--;
+        }
+
+        int entrega;
+        while (mochilaAtual > 0) {
+            entrega = encontrarEntregaMaisProxima(pai, dist);
+            if (entrega == -1) break;
+            moverPara(entrega, pai, dist);
+            entregarPedido(entrega);
         }
     }
 
-    for (int i = 0; i < N; i++) {
-        link atual = G->adj[i];
-        while (atual != NULL) {
-            link proximo = atual->next;
-            free(atual);
-            atual = proximo;
-        }
-    }
-    free(G->adj);
-    free(G);   
-    free(postos);
-    for (int m = 0; m < Q; m++)
-        free(restaurantes[m].destinoPedido);
-    free(restaurantes);
+    int pai[MAXN], dist[MAXN];
+    Dijkstra(G, posicaoAtual, pai, dist);
+    moverPara(H - 1, pai, dist);
+    encerrar();
     return 0;
 }
--- trabalho2.c
+++ trabalho2.c
@@ -1,188 +1,243 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <stdbool.h>
+
+#define MAXN 1000
 
 typedef struct node *link;
-struct node{int w, custo; link next;};
+struct node { int w, custo; link next; };
 
 typedef struct Graph *Graph;
 struct Graph {
     int v, e;
     link *adj;
+    bool *temPosto;
+    bool *temRestaurante;
 };
 
-typedef struct{
+typedef struct {
     int local, numPedidos, *destinoPedido;
-}Restaurante;
+} Restaurante;
 
 typedef struct {
     int v, w, custo;
 } Edge;
 
 Edge EDGE(int v, int w, int custo) {
-    Edge e;
-    e.v = v;
-    e.w = w;
-    e.custo = custo;
+    Edge e = {v, w, custo};
     return e;
 }
 
-link New(int w, int custo, link next){
+link New(int w, int custo, link next) {
     link x = malloc(sizeof(*x));
-
-    if(x==NULL) return NULL;
-
-    x->w = w; 
-    x->custo = custo;
-    x->next = next;
-
+    if (!x) return NULL;
+    x->w = w; x->custo = custo; x->next = next;
     return x;
 }
 
-Graph GRAPHInit(int v){
+Graph GRAPHInit(int v) {
     Graph g = malloc(sizeof(*g));
-    g->v = v;
-    g->e=0;
-    g->adj = malloc(v*sizeof(link));
-
-    for(int i=0; i < v; i++){
-        g->adj[i]=NULL;
-    }
-
+    g->v = v; g->e = 0;
+    g->adj = calloc(v, sizeof(link));
+    g->temPosto = calloc(v, sizeof(bool));
+    g->temRestaurante = calloc(v, sizeof(bool));
     return g;
 }
 
-void GRAPHInsert(Graph g, Edge e){
-    int v=e.v, w=e.w, custo=e.custo;
-    g->adj[v] = New(w, custo, g->adj[v]);
+void GRAPHInsert(Graph g, Edge e) {
+    g->adj[e.v] = New(e.w, e.custo, g->adj[e.v]);
     g->e++;
 }
 
-int moveLocal(int local){
-    return local;
+void Dijkstra(Graph G, int s, int *pa, int *dist) {
+    bool vis[MAXN] = {0};
+    for (int i = 0; i < G->v; i++) {
+        dist[i] = INT_MAX;
+        pa[i] = -1;
+    }
+    dist[s] = 0;
+
+    while (1) {
+        int u = -1;
+        for (int i = 0; i < G->v; i++) {
+            if (!vis[i] && (u == -1 || dist[i] < dist[u]))
+                u = i;
+        }
+        if (u == -1 || dist[u] == INT_MAX) break;
+        vis[u] = true;
+        for (link p = G->adj[u]; p; p = p->next) {
+            if (dist[u] + p->custo < dist[p->w]) {
+                dist[p->w] = dist[u] + p->custo;
+                pa[p->w] = u;
+            }
+        }
+    }
 }
 
-int abastecer(){
-    return 1;
-}
-
-int coletaPedido(int pedido){
-    return pedido;
-}
-
-int entregaPedido(int pedido){
-    return pedido;
-}
-
-int *postos;
+int N, M, H, T, I, C, P, Q;
 Restaurante *restaurantes;
-int capacidadeCombustivel, combustivel,
-    capacidadeMochila;
-link ultimoLocal;
+Graph G;
 
-int main(){
-    int N, M, H, T, I, C, P, Q;
-    char op;
-    int X, D;
-    Graph G;
+int mochila[MAXN], mochilaAtual = 0;
+int posicaoAtual, combustivelAtual;
 
+void emitir(const char *fmt, int x) {
+    if (x >= 0) printf(fmt, x + 1);
+    else printf("%s", fmt);
+    printf("\n");
+    fflush(stdout);
+}
+
+int ler() {
+    int r; scanf("%d", &r); return r;
+}
+
+bool moverPara(int destino, int *pai, int *dist) {
+    if (dist[destino] > combustivelAtual) {
+        int melhorPosto = -1, menorCusto = INT_MAX;
+        for (int i = 0; i < N; i++) {
+            if (!G->temPosto[i]) continue;
+            if (dist[i] <= combustivelAtual && dist[i] < menorCusto) {
+                melhorPosto = i;
+                menorCusto = dist[i];
+            }
+        }
+        if (melhorPosto != -1) {
+            moverPara(melhorPosto, pai, dist);
+            emitir("a", -1);
+            combustivelAtual = T;
+            return moverPara(destino, pai, dist);
+        }
+        return false;
+    }
+
+    if (pai[destino] != -1) moverPara(pai[destino], pai, dist);
+    emitir("m %d", destino);
+    int r = ler();
+    if (r == 1) {
+        for (link a = G->adj[posicaoAtual]; a; a = a->next)
+            if (a->w == destino) combustivelAtual -= a->custo;
+        posicaoAtual = destino;
+
+        if (G->temPosto[posicaoAtual]) {
+            emitir("a", -1);
+            combustivelAtual = T;
+            ler();
+        }
+
+        return true;
+    }
+    return false;
+}
+
+bool pegarPedido(int destino) {
+    emitir("p %d", destino);
+    int r = ler();
+    if (r == 1) mochila[mochilaAtual++] = destino;
+    return r == 1;
+}
+
+bool entregarPedido(int destino) {
+    emitir("e %d", destino);
+    int r = ler();
+    if (r == 1) {
+        for (int i = 0; i < mochilaAtual; i++)
+            if (mochila[i] == destino) {
+                for (int j = i; j < mochilaAtual - 1; j++)
+                    mochila[j] = mochila[j + 1];
+                mochilaAtual--;
+                break;
+            }
+    }
+    return r == 1;
+}
+
+void encerrar() {
+    emitir("x", -1);
+    ler();
+}
+
+int destinoMaisProximo(int *dist) {
+    int melhor = -1, melhorCusto = INT_MAX;
+    for (int i = 0; i < Q; i++) {
+        if (restaurantes[i].numPedidos > 0) {
+            int local = restaurantes[i].local;
+            if (dist[local] < melhorCusto) {
+                melhorCusto = dist[local];
+                melhor = i;
+            }
+        }
+    }
+    return melhor;
+}
+
+int main() {
     scanf("%d %d %d %d %d %d %d", &N, &M, &H, &T, &I, &C, &P);
-    capacidadeCombustivel = T;
-    combustivel = I;
-    capacidadeMochila = C;
-
     G = GRAPHInit(N);
-    if(!G){
-        return -1;
+    for (int i = 0; i < P; i++) {
+        int p; scanf("%d", &p); G->temPosto[p - 1] = true;
     }
-
-    postos = malloc(P * sizeof(int));
-    if(!postos){
-        return -1;
-    }
-    for(int i = 0; i < P; i++){
-        postos[i] = 0;
-    }
-
-    // postos
-    int posto = -1;
-    for(int i = 0; i < P; i++){
-        scanf("%d", &posto);
-        postos[posto] = 1;
-    }
-    int u, v, w;
-    //ruas
-    for(int j = 0; j < M; j++){
+    for (int i = 0; i < M; i++) {
+        int u, v, w;
         scanf("%d %d %d", &u, &v, &w);
-        GRAPHInsert(G, EDGE(u, v, w));
+        GRAPHInsert(G, EDGE(u - 1, v - 1, w));
     }
-
-    // qtde restaurantes
     scanf("%d", &Q);
     restaurantes = malloc(Q * sizeof(Restaurante));
-    if(!restaurantes){
-        return -1;
+    for (int i = 0; i < Q; i++) {
+        int r, k; scanf("%d %d", &r, &k);
+        G->temRestaurante[r - 1] = true;
+        restaurantes[i].local = r - 1;
+        restaurantes[i].numPedidos = k;
+        restaurantes[i].destinoPedido = malloc(k * sizeof(int));
+        for (int j = 0; j < k; j++) {
+            scanf("%d", &restaurantes[i].destinoPedido[j]);
+            restaurantes[i].destinoPedido[j]--;
+        }
     }
-    // dados resturante
-    int r, k;
-    // local restaurante, numero pedidos, destino pedidos
-    for(int m = 0; m < Q; m++){
-        scanf("%d %d", &r, &k);
-        restaurantes[m].local = r;
-        restaurantes[m].numPedidos = k;
-        restaurantes[m].destinoPedido = malloc(k*sizeof(int));
-        for(int n = 0; n < k; n++)
-            scanf("%d", &restaurantes[m].destinoPedido[n]);
-    }
-    
-    while(1){
-        // Menu de opções
-        scanf(" %c", &op);
-        int res;
-        switch (op)
-        {
-            case 'm':
-                scanf("%d", &X);
-                // Função de mover para X
-                res = moveLocal(X);
-                printf("%d", res);
-                break;
-            case 'a':
-                // Função de abastecer
-                res = abastecer();
-                printf("%d", res);
-                break;
-            case 'p':
-                scanf("%d", &D);
-                // Função pegar pedido D
-                res = coletaPedido(D);
-                printf("%d", res);
-                break;
-            case 'o':
-                scanf("%d", &D);
-                // Função entregar pedido no local D
-                res = entregaPedido(D);
-                printf("%d", res);
-                break;
-            case 'x':
-                // Função de encerrar a entrega
-                printf("1 \n");
-                return 0;
+    posicaoAtual = H - 1;
+    combustivelAtual = I;
+
+    while (1) {
+        int pai[MAXN], dist[MAXN];
+        Dijkstra(G, posicaoAtual, pai, dist);
+
+        int idx = destinoMaisProximo(dist);
+        if (idx == -1) break;
+        int rlocal = restaurantes[idx].local;
+        moverPara(rlocal, pai, dist);
+
+        while (restaurantes[idx].numPedidos > 0 && mochilaAtual < C) {
+            int melhorD = -1, melhorDist = INT_MAX;
+            for (int i = 0; i < restaurantes[idx].numPedidos; i++) {
+                int d = restaurantes[idx].destinoPedido[i];
+                Dijkstra(G, rlocal, pai, dist);
+                if (dist[d] < melhorDist) {
+                    melhorDist = dist[d];
+                    melhorD = i;
+                }
+            }
+            if (melhorD == -1) break;
+            int destino = restaurantes[idx].destinoPedido[melhorD];
+            if (!pegarPedido(destino)) break;
+            for (int j = melhorD + 1; j < restaurantes[idx].numPedidos; j++)
+                restaurantes[idx].destinoPedido[j - 1] = restaurantes[idx].destinoPedido[j];
+            restaurantes[idx].numPedidos--;
+        }
+
+        for (int i = 0; i < mochilaAtual;) {
+            int d = mochila[i];
+            Dijkstra(G, posicaoAtual, pai, dist);
+            moverPara(d, pai, dist);
+            if (entregarPedido(d)) continue;
+            i++;
         }
     }
 
-    for (int i = 0; i < N; i++) {
-        link atual = G->adj[i];
-        while (atual != NULL) {
-            link proximo = atual->next;
-            free(atual);
-            atual = proximo;
-        }
-    }
-    free(G->adj);
-    free(G);   
-    free(postos);
-    for (int m = 0; m < Q; m++)
-        free(restaurantes[m].destinoPedido);
-    free(restaurantes);
+    int pai[MAXN], dist[MAXN];
+    Dijkstra(G, posicaoAtual, pai, dist);
+    moverPara(H - 1, pai, dist);
+    encerrar();
     return 0;
 }
